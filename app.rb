f88a3b5c5dfd4aa626f810604f6499aaf4ecfbef
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-09 03:48:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+
+	get '/info' do
+		"Testing the info page"
+	end
 end
 # how would I implement a string that looks like html to my
 # browser without having it changed to actual html??
-
-get '/info' do 
-	"Testing the info page"
-end
